1dce34645b07455279f417eb9bfc105ea4ff7c67
cmcd22 <chrismcdonald907@gmail.com>
2025-08-25 11:13:15 +1200

    - Added get_api function to make a request to Spotify API and return the response - Defined base URL for API calls

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ load_dotenv()
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
+BASE = "https://api.spotify.com/v1"
+
 def get_token():
     """
     Make a request to Spotify API and returns the access token
@@ -18,3 +20,19 @@ def get_token():
     )
     r.raise_for_status()
     return r.json()["access_token"]
+
+def get_api(path, token, params=None):
+    """
+    Make a request to Spotify API and returns the API response
+    """
+    path = path if path.startswith("/") else f"/{path}"
+    url = f"{BASE}{path}"
+    headers = {"Authorization": f"Bearer {token}"}
+
+    while True:
+        resp = requests.get(url, headers=headers, params=params)
+        if resp.status_code == 200:
+            return resp.json()
+        else:
+            print(resp.status_code)
+            return None
